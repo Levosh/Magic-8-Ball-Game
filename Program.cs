d829d1c26d99967fee947acfe14949db5ba9ab2b
Levosh <pav.toshev@gmail.com>
2017-04-05 19:13:33 +0300

    Magic 8-ball Game

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        String a = "FOR";
+        String b = "THE";
+        object c = "HORDE";
+        string Horde = string.Format("In WOW you are {0} {1} {2}!!!", a, b, c);
+
+        Console.WriteLine(Horde);
+
+        c = 5;
+        Horde = string.Format("In WOW you are {0} {1} {2}!!!", a, b, c);
+
+        Console.WriteLine(Horde);
+
+    }
+}
